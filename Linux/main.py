7992cc7e160810f8c69ba19544536077df5bcc12
Erfan ram <Erfanramezany245@gmail.com>
2023-04-19 17:57:26 +0330

    added find pattern

--- Linux/main.py
+++ Linux/main.py
@@ -1,7 +1,19 @@
-with open("/etc/hosts", "a+") as file:
-    file.seek(0)                # set cursor to the beginning of the file
-    print(file.read())          # read and print all text in the file
+with open("/etc/hosts", "r") as check:
+    # data = check.read()
+    lines = check.readlines()
 
-    file.write("\n# mamnually written")
-    file.write("\n142.250.184.206 google.com")
-    file.write("\n142.250.184.206 www.google.com")
+    print(lines)
+    for line in lines:
+        if 'google.com' in line.lower() or 'www.google.com' in line.lower():
+            print("found some settings")
+        else:
+            print("nothing found")
+
+
+# with open("/etc/hosts", "a+") as file:
+#     file.seek(0)
+#     print(file.read())
+
+#     file.write("\n# mamnually written")
+#     file.write("\n142.250.184.206 google.com")
+#     file.write("\n142.250.184.206 www.google.com")
